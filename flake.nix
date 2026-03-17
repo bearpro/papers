d6dea89f057a06d0de13c90be18c4f9e1cca6253
mprokazin <bearpro@outlook.com>
2026-03-17 17:07:22 +0300

    Добавлен flake с pandoc

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Dev shell with pandoc";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          });
+    in {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            pandoc
+          ];
+        };
+      });
+    };
+}
